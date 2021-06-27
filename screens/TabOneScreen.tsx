e8c37941259fe645912a51d63c02a23744c7eaa3
Benjamin Coux <benjamin.coux@etu.unice.fr>
2021-06-27 20:26:32 +0100

    ajout paramêtres

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native'
 import {Game} from "../components/Game";
 
@@ -7,6 +7,7 @@ import {Game} from "../components/Game";
 export default function TabOneScreen(){
 
     var gameList =["Fuck the queen","Dealer","Ben Laden","Jeu de carte simple"]
+    let paramPath = require("../assets/images/params.png")
 
     const navigation = useNavigation()
 
@@ -15,8 +16,14 @@ export default function TabOneScreen(){
         navigation.navigate('HomeScreen',{})
     }
 
+
     return(
-    <View style={{backgroundColor :"#0F3052", flex :1 ,alignItems:"center"}}>
+    <View style={{backgroundColor :"#0F3052", flex :1 ,}}>
+        <View style={{flex:1,flexDirection:"row-reverse",height:"10%"}}>
+            <TouchableOpacity>
+                <Image style={{height:50,width:50}} source={paramPath}/>
+            </TouchableOpacity>
+        </View>
         <FlatList  numColumns={2} data={gameList} renderItem={({item}) =>
                 <Game name={item}/>
         }/>
